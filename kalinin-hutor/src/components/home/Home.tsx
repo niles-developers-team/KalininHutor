a45cecbec516694d5bbb50577490872fda3819a4
Kirill Ivanchenko <K.Ivanchenko@treolan.ru>
2022-06-14 04:21:31 +0300

    #UPDT: Много всяких фиксов

---
+++ kalinin-hutor/src/components/home/Home.tsx
@@ -0,0 +1,18 @@
+import { Container, Grid, CircularProgress } from "@mui/material";
+import { useState } from "react";
+import { RouteProps } from "react-router-dom";
+import './Home.scss';
+
+interface Props extends RouteProps {}
+
+export const HomeComponent = function(props: Props) {
+    const [loading, setLoading] = useState<boolean>(false);
+    
+    return (
+        <Container maxWidth="sm">
+            <Grid container direction="column" component="main" alignItems="center" justifyContent="start">
+                {loading && <CircularProgress />}
+            </Grid>
+        </Container>
+    );
+}
--- kalinin-hutor/src/models/rentalObject.ts
+++ kalinin-hutor/src/models/rentalObject.ts
@@ -1,8 +1,8 @@
 export interface RentalObject {
     // Идентификатор объекта аренды
-    id: string;
+    id: string | null;
     // Идентификатор владельца
-    landlordId: string;
+    landlordId: string | null;
     // Название объекта аренды 
     name: string;
     // Описание объекта аренды
@@ -13,37 +13,47 @@ export interface RentalObject {
     checkoutTime: string;
 }
 
-//Очередь получения объектов аренды
-export interface GetRentalObjectsQuery
-{
-    id?: string;
-    // Идентификатор владельца
-    landlordId?: string;
-    //Строка поиска
-    searchText?: string;
-    //Время заезда
-    checkinTime?: string;
-    //Время отъезда
-    checkoutTime?: string;
-}
+export namespace RentalObject {
+    //Очередь получения объектов аренды
+    export interface GetQuery {
+        id?: string;
+        // Идентификатор владельца
+        landlordId?: string;
+        //Строка поиска
+        searchText?: string;
+        //Время заезда
+        checkinTime?: string;
+        //Время отъезда
+        checkoutTime?: string;
+    }
 
-export interface CreateRentalObjectRequest {
-    name: string;
-    description: string;
-    address: string;
-    landlordId: string;
-    checkinTime: string;
-    checkoutTime: string;
-}
+    export interface CreateRequest {
+        name: string;
+        description: string;
+        address: string;
+        landlordId: string;
+        checkinTime: string;
+        checkoutTime: string;
+    }
 
-export interface UpdateRentalObjectRequest {
-    id: string;
-    name: string;
-    description: string;
-    checkinTime: string;
-    checkoutTime: string;
-}
+    export interface UpdateRequest {
+        id: string;
+        name: string;
+        description: string;
+        checkinTime: string;
+        checkoutTime: string;
+    }
 
-export interface DeleteRentalObjectRequest {
-    id: string;
+    export interface DeleteRequest {
+        id: string;
+    }
+
+    export const initial: RentalObject = {
+        id: null,
+        checkinTime: '12:00:00',
+        checkoutTime: '12:00:00',
+        description: '',
+        landlordId: null,
+        name: ''
+    }
 }
