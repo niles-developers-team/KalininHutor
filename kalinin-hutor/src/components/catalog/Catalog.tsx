def7fde78c3eb0660991f1c23c566818d3330c17
Kirill Ivanchenko <K.Ivanchenko@treolan.ru>
2023-06-13 00:24:23 +0300

    #UPDT: Добавил заголовок на страницу каталога

--- kalinin-hutor/src/components/catalog/Catalog.tsx
+++ kalinin-hutor/src/components/catalog/Catalog.tsx
@@ -1,5 +1,9 @@
 import { Grid } from "@mui/material"
+import { appName } from "../.."
 
 export const CatalogComponent = function (): JSX.Element {
+
+    document.title = appName;
+
     return (<Grid></Grid>)
 }
