50ebe0037b5c70a8b5a355421b4a2baa6cd4dc1b
Cheweee <kni2012.15@gmail.com>
2023-07-24 01:24:44 +0300

    #UPDT: Добавил маппинг, обработку и выгрузку координат объекта аренды

---
+++ KalininHutor.API/DTO/Common/CoordinatesDTO.cs
@@ -0,0 +1,10 @@
+namespace KalininHutor.API.DTO;
+
+///<summary> Координаты </summary>
+public class CoordinatesDTO
+{
+    ///<summary> Широта </summary>
+    public float Latitude { get; set; }
+    ///<summary> Долгота </summary>
+    public float Longitude { get; set; }
+}
